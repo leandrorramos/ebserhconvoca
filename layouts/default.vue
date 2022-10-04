83e92dad5b270492568c4b3d5ee5b5f968a9cbb1
leandrorramos <leandroramos87@gmail.com>
2022-10-04 13:30:37 -0300

    correção bug verificação adsense. atualização nuxt rc 11

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="graylight">
+        <NuxtPage />
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style >
+.graylight{
+    background-color: azure;
+    margin: 0 0;
+    padding: 0 0;
+    height: 100%;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,14 @@
-import { defineNuxtConfig } from 'nuxt'
-
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    head:{
-        script:[
-            {
-                src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7491649292131125",
-                async: true,
-                crossorigin: "anonymous"
-            }
-        ]
+    app:{
+        head:{
+            script:[
+                {
+                    src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7491649292131125",
+                    async: true,
+                    crossorigin: "anonymous"
+                }
+            ]
+        }
     }
 })
-
-
-//<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7491649292131125" crossorigin="anonymous"></script>
